557b7ae7bed928447d239facfa33df635a3e4154
Kisanagi <dhaniwahyu15w@gmail.com>
2026-06-27 15:48:59 +0700

    Fix URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'https://si-ilab.vercel.app',
+  'https://si-ilab.up.railway.app',
   'http://localhost:5173',
 ];
 app.use(cors({
